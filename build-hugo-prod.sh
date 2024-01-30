2a5c291d926775cdb9715a4572ab156f6e5f8b47
jon-hatfield <>
2024-01-30 14:04:47 +0000

    google analytics

--- build-hugo-prod.sh
+++ build-hugo-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf docs/*
-hugo --baseURL https://jon-hatfield-tech-writing.github.io/sekb-hugo-docs
+env HUGO_GOOGLEANALYTICS=$SEKB_HUGO_GA hugo --baseURL https://jon-hatfield-tech-writing.github.io/sekb-hugo-docs
 touch docs/.nojekyll
